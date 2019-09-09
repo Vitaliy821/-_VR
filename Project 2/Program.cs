af869e1f9aafc17c65e72402ea85571986535b4e
Vitaliy821 <vitaliy3714@gmail.com>
2019-09-09 22:56:43 +0300

    Add project files.

---
+++ Project 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i;
+            double s = 0;
+            int a1;
+            int a2;
+            do {
+                Console.WriteLine("Input first num:");
+                int.TryParse(Console.ReadLine(), out a1);
+                Console.WriteLine("Input second num:");
+                int.TryParse(Console.ReadLine(), out a2);
+                if (a2 > a1)
+                {
+                    for (i = a1; i < a2; i++)
+                    {
+                        s += (Math.Pow(i, 2) - 1) / ((Math.Pow(-1, i + 1) * Math.Pow(i, 2) + 7));
+                    }
+                    Console.WriteLine("S={0}", s);
+                }
+                else
+                    Console.WriteLine("Error!");
+            } while (a2 < a1);
+                    
+            Console.ReadKey();
+        }
+    }
+}
