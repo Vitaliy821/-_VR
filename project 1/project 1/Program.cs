22d4ce1e789ee84623752013a2ab5eff9fbe1825
Vitaliy821 <vitaliy3714@gmail.com>
2019-09-10 10:02:28 +0300

    laba

---
+++ project 1/project 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     
+            double r;
+            Console.WriteLine("Input r1:");
+            double.TryParse(Console.ReadLine(), out double r1);
+            Console.WriteLine("Input r2:");
+            double.TryParse(Console.ReadLine(), out double r2);
+            r = (r1*r2)/(r1+r2);
+            Console.WriteLine( "R={0}", r);
+            Console.ReadKey();
+        }
+    }
+}
