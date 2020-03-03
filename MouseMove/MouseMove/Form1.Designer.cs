4e8b8814c2add6fb030f711f4d6a176c036ede2d
Vitaliy <vitaliy3714@gmail.com>
2020-03-03 22:14:29 +0200

    Lab2

---
+++ MouseMove/MouseMove/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace MouseMove
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(342, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 300;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(342, 192);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(100, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "CLOSE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MouseMove/MouseMove/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseMove
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+   
+            this.MouseMove += Form1_MouseMove;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+
+        {
+            int delta = 50;
+            int speed = 2;
+            int invis = 100;
+
+            if (button1.Location.X + button1.Width >= this.Width)
+            {
+                button1.Location = new Point(5, button1.Location.Y);
+            }
+            else if (button1.Location.Y + button1.Height >= this.Height)
+            {
+                button1.Location = new Point(button1.Location.X, 5);
+            }
+            else if (button1.Location.X <= 0)
+            {
+                button1.Location = new Point(this.Width - button1.Width-delta, button1.Location.Y);
+            }
+            else if (button1.Location.Y <= 0)
+            {
+                button1.Location = new Point(button1.Location.X, this.Height - button1.Height-delta);
+            }
+            else if (e.X > button1.Location.X - delta && e.X < button1.Location.X && e.Y > button1.Location.Y - delta && e.Y < button1.Location.Y)
+            {
+                button1.Location = new Point(button1.Location.X + speed, button1.Location.Y + speed);
+            }
+            else if (e.X < button1.Location.X + delta && e.X > button1.Location.X && e.Y > button1.Location.Y - delta && e.Y < button1.Location.Y)
+            {
+                button1.Location = new Point(button1.Location.X, button1.Location.Y + speed);     
+                button1.Height--;
+            }
+            else if (e.X < button1.Location.X + button1.Width + delta && e.X > button1.Location.X && e.Y > button1.Location.Y - delta && e.Y < button1.Location.Y)
+            {
+                button1.Location = new Point(button1.Location.X - speed, button1.Location.Y + speed);                
+            }
+            else if (e.X < button1.Location.X + button1.Width + delta && e.X > button1.Location.X && e.Y > button1.Location.Y && e.Y < button1.Location.Y + button1.Height)
+            {
+                button1.Location = new Point(button1.Location.X - speed, button1.Location.Y);               
+            }
+            else if (e.X < button1.Location.X + button1.Width + delta && e.X > button1.Location.X + button1.Width && e.Y < button1.Location.Y + button1.Height + delta && e.Y > button1.Location.Y + button1.Height)
+            {
+                button1.Location = new Point(button1.Location.X - speed, button1.Location.Y - speed);               
+            }
+            else if (e.X < button1.Location.X + button1.Width && e.X > button1.Location.X && e.Y < button1.Location.Y + button1.Height + delta && e.Y > button1.Location.Y + button1.Height)
+            {
+                button1.Location = new Point(button1.Location.X, button1.Location.Y - speed);
+                button1.Height--;                
+            }
+            else if (e.X > button1.Location.X - delta && e.X < button1.Location.X && e.Y < button1.Location.Y + button1.Height + delta && e.Y > button1.Location.Y + button1.Height)
+            {
+                button1.Location = new Point(button1.Location.X + speed, button1.Location.Y - speed);                  
+            }
+            else if (e.X > button1.Location.X - delta && e.X < button1.Location.X && e.Y > button1.Location.Y + button1.Height - delta && e.Y < button1.Location.Y + button1.Height)
+            {
+                button1.Location = new Point(button1.Location.X + speed, button1.Location.Y);             
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = "Click button 'ok'";
+            timer1.Enabled = false;
+            timer2.Enabled = true;
+        }  
+        
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.Text = "";
+            timer1.Enabled = true;
+            timer2.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+}
