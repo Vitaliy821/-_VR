548a066cd25036bbb2f671c0cb73b8df4abf4c45
Vitaliy <vitaliy3714@gmail.com>
2020-04-28 22:48:27 +0300

    Work with registry

---
+++ work_with_register/work_with_register/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace work_with_register
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 42);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(130, 42);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(98, 21);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Location = new System.Drawing.Point(50, 50);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ work_with_register/work_with_register/Form1.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace work_with_register
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormClosing += Form1_FormClosing;
+            /*XML
+            textBox1.Text = Properties.Settings.Default.text_box;
+            checkBox1.Checked = Properties.Settings.Default.check_box1;
+            checkBox2.Checked = Properties.Settings.Default.check_box2;
+            this.Width = Properties.Settings.Default.width;
+            this.Height = Properties.Settings.Default.height;
+            this.Location = new Point(Properties.Settings.Default.x, Properties.Settings.Default.y);
+            Properties.Settings.Default.Save();
+            */
+
+
+            /*TXT
+            System.IO.File.AppendAllText(@"D:\\C#_works\\work_with_register\\file1.txt", null);
+            System.IO.StreamReader file = new System.IO.StreamReader(@"D:\\C#_works\\work_with_register\\file1.txt");
+            using (file)
+            {
+                try
+                {
+                    textBox1.Text = file.ReadLine(); ;
+                    checkBox1.Checked = Convert.ToBoolean(file.ReadLine());
+                    checkBox2.Checked = Convert.ToBoolean(file.ReadLine());
+                    this.Width = Int32.Parse(file.ReadLine());
+                    this.Height = Int32.Parse(file.ReadLine());
+                    this.Location = new Point(Int32.Parse(file.ReadLine()), Int32.Parse(file.ReadLine()));
+                }
+                catch { }
+
+            }
+            */
+
+            /*BINARY
+            string path = @"D:\\C#_works\\work_with_register\\states.dat";
+            using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+            {
+               
+                while (reader.PeekChar() > -1)
+                {
+                    textBox1.Text = reader.ReadString();
+                    checkBox1.Checked = reader.ReadBoolean();
+                    checkBox2.Checked = reader.ReadBoolean();
+                    this.Width = reader.ReadInt32();
+                    this.Height = reader.ReadInt32();
+                    this.Location = new Point(reader.ReadInt32(), reader.ReadInt32());
+                }
+            }
+            */
+
+            /*REGISTRY*/
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\myProgramm");
+            textBox1.Text = key.GetValue("textBox1").ToString();
+            checkBox1.Checked = Convert.ToBoolean(key.GetValue("checkBox1"));
+            checkBox2.Checked = Convert.ToBoolean(key.GetValue("checkBox2"));
+            this.Width = Convert.ToInt32(key.GetValue("Width"));
+            this.Height = Convert.ToInt32(key.GetValue("Height"));
+            this.Location = new Point(Convert.ToInt32(key.GetValue("X")), Convert.ToInt32(key.GetValue("Y")));
+            key.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, EventArgs e)
+        {
+            /*XML
+            Properties.Settings.Default.text_box = this.textBox1.Text;
+            Properties.Settings.Default.check_box1 = this.checkBox1.Checked;
+            Properties.Settings.Default.check_box2 = this.checkBox2.Checked;
+            Properties.Settings.Default.width = this.Width;
+            Properties.Settings.Default.height = this.Height;
+            Properties.Settings.Default.x = this.Location.X;
+            Properties.Settings.Default.y = this.Location.Y;
+            Properties.Settings.Default.Save();
+            */
+            /*TXT
+            string path = @"D:\\C#_works\\work_with_register\\file1.txt";
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+
+                sw.WriteLine(textBox1.Text);
+                sw.WriteLine(this.checkBox1.Checked);
+                sw.WriteLine(this.checkBox2.Checked);
+                sw.WriteLine(this.Width);
+                sw.WriteLine(this.Height);
+                sw.WriteLine(this.Location.X);
+                sw.WriteLine(this.Location.Y);
+            }
+            */
+            /*BINARY
+             string path = @"D:\\C#_works\\work_with_register\\states.dat";
+                using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+                {
+                    writer.Write(textBox1.Text);
+                    writer.Write(this.checkBox1.Checked);
+                    writer.Write(this.checkBox2.Checked);
+                    writer.Write(this.Width);
+                    writer.Write(this.Height);
+                    writer.Write(this.Location.X);
+                    writer.Write(this.Location.Y);
+                }
+            */
+            /*REGISTRY*/
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\myProgramm");
+            key.SetValue("textBox1", textBox1.Text);
+            key.SetValue("checkBox1", this.checkBox1.Checked);
+            key.SetValue("checkBox2", this.checkBox2.Checked);
+            key.SetValue("Width", this.Width);
+            key.SetValue("Height", this.Height);
+            key.SetValue("X", this.Location.X);
+            key.SetValue("Y", this.Location.Y);
+            key.Close();
+        }
+    }
+}
