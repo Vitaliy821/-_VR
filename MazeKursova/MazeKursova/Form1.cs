199c29ef7cc4a1c677827eb4ad2bb9e0d721a213
Vitaliy <vitaliy3714@gmail.com>
2020-06-09 17:48:36 +0300

    Kursova

---
+++ MazeKursova/MazeKursova/Form1.cs
@@ -0,0 +1,151 @@
+﻿using Maze_kursova;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MazeKursova
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private int CellWid, CellHgt;
+        Maze inMaze = new Maze(10, 10);
+        Bitmap inBm = new Bitmap(1, 1);
+
+       
+
+
+
+        private void createBtn_Click(object sender, EventArgs e)
+        {
+            
+            int width = 0;
+            int hight = 0;
+
+            try
+            {
+                width = int.Parse(txtWidth.Text);
+                hight = int.Parse(txtHeight.Text);
+                //перевірка розмірів
+                if (width <= 0 || hight <= 0)
+                {
+                    throw new FormatException();
+                }
+
+            }
+            catch (System.FormatException)
+            {
+                string message = "The dimension must be a number greater than 0.";
+                string caption = "Error";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+                result = MessageBox.Show(message, caption, buttons);
+                txtWidth.Text = "10";
+                txtHeight.Text = "10";
+
+                return;
+            }
+
+
+            int oddW = 0;
+            int oddH = 0;
+            //для випадку з непарними розмірами 
+            if (width % 2 != 0 && width != 0)
+            {
+                oddW = 1;
+            }
+            if (width % 2 != 0 && hight != 0)
+            {
+                oddH = 1;
+            }
+
+            //розраховується ширина і висота клітинки для маштабування
+            CellWid = picMaze.ClientSize.Width / (width + 2);
+            CellHgt = picMaze.ClientSize.Height / (hight + 2);
+
+           
+            int CellMin = 10;
+            if (CellWid < CellMin)
+            {
+                CellWid = CellMin;
+                CellHgt = CellWid;
+            }
+            else if (CellHgt < CellMin)
+            {
+                CellHgt = CellMin;
+                CellWid = CellHgt;
+            }
+            else if (CellWid > CellHgt) CellWid = CellHgt;
+            else CellHgt = CellWid;
+
+
+            Maze maze = new Maze(width, hight);
+
+           
+            maze.finish.X = maze.finish.X + oddW;
+            maze.finish.Y = maze.finish.Y + oddH;
+            maze.CreateMaze();
+            DrawMaze();
+
+            inMaze = maze;
+
+            void DrawMaze()
+            {
+                inBm.Dispose();
+                
+                Bitmap bm = new Bitmap(
+                    CellWid * (maze.finish.X + 2),
+                    CellHgt * (maze.finish.Y + 2), System.Drawing.Imaging.PixelFormat.Format16bppRgb555);
+
+                Brush whiteBrush = new SolidBrush(Color.White);
+                Brush blackBrush = new SolidBrush(Color.Black);
+
+                using (Graphics gr = Graphics.FromImage(bm))
+                {
+
+                    gr.SmoothingMode = SmoothingMode.AntiAlias;
+                    for (var i = 0; i < maze._cells.GetUpperBound(0) + oddW; i++)
+                        for (var j = 0; j < maze._cells.GetUpperBound(1) + oddH; j++)
+                        {
+                            Point point = new Point(i * CellWid, j * CellWid);
+                            Size size = new Size(CellWid, CellWid);
+                            Rectangle rec = new Rectangle(point, size);
+                            if (maze._cells[i, j]._isCell)
+                            {
+                                gr.FillRectangle(whiteBrush, rec);
+                            }
+                            else
+                            {
+
+                                gr.FillRectangle(blackBrush, rec);
+                            }
+                        }
+
+                    gr.FillRectangle(new SolidBrush(Color.Green), 
+                        new Rectangle(new Point(maze.start.X * CellWid, maze.start.Y * CellWid),
+                        new Size(CellWid, CellWid)));
+                    gr.FillRectangle(new SolidBrush(Color.Red), 
+                        new Rectangle(new Point(maze.finish.X * CellWid, maze.finish.Y * CellWid),
+                        new Size(CellWid, CellWid)));
+                }
+
+                picMaze.Image = bm;
+                inBm = bm;
+
+            }
+        }
+
+    }
+}
---
+++ MazeKursova/MazeKursova/Maze.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+
+namespace Maze_kursova
+{
+
+    class Maze
+    {
+        public readonly Cell[,] _cells;
+        private int _width;
+        private int _height;
+        public Stack<Cell> _path = new Stack<Cell>();
+        public List<Cell> _neighbours = new List<Cell>();
+        public Random rnd = new Random();
+        public Cell start;
+        public Cell finish;
+        public Maze(int width, int height)
+        {
+            start = new Cell(1, 1, true, true);
+            finish = new Cell(width - 3, height - 3, true, true);
+            _width = width;
+            _height = height;
+            _cells = new Cell[width, height];
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < height; j++)
+                {
+                    if ((i % 2 != 0 && j % 2 != 0) && (i < _width - 1 && j < _height - 1)) 
+                    {
+                        _cells[i, j] = new Cell(i, j); 
+                    }
+                    else
+                    {
+                        _cells[i, j] = new Cell(i, j, false, false);
+                    }
+                }
+            }
+            _path.Push(start);
+            
+        }
+
+
+        public void CreateMaze()
+        {
+            
+            while (_path.Count != 0) 
+            {
+                _neighbours.Clear();
+                GetNeighbours(_path.Peek());
+                if (_neighbours.Count != 0)
+                {
+                    Cell nextCell = ChooseNeighbour(_neighbours);
+                    RemoveWall(_path.Peek(), nextCell);
+                    nextCell._isVisited = true; 
+                    _cells[nextCell.X, nextCell.Y]._isVisited = true; 
+                    _path.Push(nextCell);
+
+                }
+                else
+                {
+                    _path.Pop();
+                }
+
+            }
+        }
+
+        private void GetNeighbours(Cell localcell)
+        {
+
+            int x = localcell.X;
+            int y = localcell.Y;
+            const int distance = 2;
+            Cell[] possibleNeighbours = new[] 
+            {
+                new Cell(x, y - distance), // Up
+                new Cell(x + distance, y), // Right
+                new Cell(x, y + distance), // Down
+                new Cell(x - distance, y) // Left
+            };
+            for (int i = 0; i < 4; i++) 
+            {
+                Cell curNeighbour = possibleNeighbours[i];
+                if (curNeighbour.X > 0 && curNeighbour.X < _width && curNeighbour.Y > 0 && curNeighbour.Y < _height)
+                {
+                    if (_cells[curNeighbour.X, curNeighbour.Y]._isCell && !_cells[curNeighbour.X, curNeighbour.Y]._isVisited)
+                    {
+                        _neighbours.Add(curNeighbour);
+                    }
+                }
+            }
+
+        }
+
+        private Cell ChooseNeighbour(List<Cell> neighbours)
+        {
+
+            int r = rnd.Next(neighbours.Count);
+            return neighbours[r];
+
+        }
+
+        private void RemoveWall(Cell first, Cell second)
+        {
+            int xDiff = second.X - first.X;
+            int yDiff = second.Y - first.Y;
+            int addX = (xDiff != 0) ? xDiff / Math.Abs(xDiff) : 0; // Наппрям видалення стіни
+            int addY = (yDiff != 0) ? yDiff / Math.Abs(yDiff) : 0;
+
+            // Координати видаленої стіни
+            _cells[first.X + addX, first.Y + addY]._isCell = true; //обращаем стену в клетку
+            _cells[first.X + addX, first.Y + addY]._isVisited = true; //клітинка відвідана 
+            second._isVisited = true; //делаем клетку посещенной
+            _cells[second.X, second.Y] = second;
+
+        }
+    }
+}
